26a95e28ff656c63570234299575ad55985599a6
RizkiRahardiputra <42662883+RizkiRahardiputra@users.noreply.github.com>
2018-09-13 08:57:46 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,65 @@
 <?php
+echo 'Soal 1 : <br> <br>';
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$nilai1 = 5;
+
+function Piramida($x = 1){
+global $nilai1;
+echo "*";
+ $x++;
+if ($x <= $nilai1) {
+	Piramida($x);
+
 }
-rekursif(5);
+else{
+	echo "<br>";
+	$nilai1--;
+	if($nilai1 != 0){
+		Piramida();
+	}
+
+
+}
+
+
+}
+Piramida();
+
+echo "<br>";
+
+//////////////////////////////////////////////////
+
+echo 'Soal 2 : <br> <br>';
+
+$nilai2 = 1;
+
+function Piramida2 ($y = 1){
+	global $nilai2;
+	echo "$y";
+	$y++;
+
+	if ($y <= $nilai2) {
+		
+		# code...
+
+		Piramida2($y);
+	}
+
+	else{
+		echo "<br>";
+		$nilai2++;
+		if ($nilai2 <= 5) {
+			# code...
+			Piramida2();
+		}
+
+	}
+}
+
+Piramida2();
+
+
+
+
+
 ?>
